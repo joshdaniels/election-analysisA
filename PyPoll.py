dce67c61047cafc7eb17dd31e37774023bcb35b8
Josh Daniels <joshdaniels@Joshs-MBP.attlocal.net>
2021-09-26 18:32:23 -0500

    adding election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -81,7 +81,15 @@ with open(file_to_load) as election_data:
 
             # To do: print out each candidate's name, vote count, and percentage of
             # votes to the terminal.
+
+            candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
             #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+            # Print each candidate, their voter count, and percentage to the terminal.
+            print(candidate_results)
+
+            #  Save the candidate results to our text file.
+            txt_file.write(candidate_results)
 
             # Determine winning vote count and candidate
             # Determine if the votes are greater than the winning count.
@@ -95,12 +103,15 @@ with open(file_to_load) as election_data:
                 winning_candidate = candidate_name
 
         winning_candidate_summary = (
-        f"-------------------------\n"
-        f"Winner: {winning_candidate}\n"
-        f"Winning Vote Count: {winning_count:,}\n"
-        f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"-------------------------\n")
+            f"-------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-------------------------\n")
         #print(winning_candidate_summary)
+
+        # Save the winning candidate's name to the text file.
+        txt_file.write(winning_candidate_summary)
                 
 
         # if statement to only show unique candidate names
